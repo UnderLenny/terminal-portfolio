d10fcaec91c5292c3b6f24aeaaf98cc663edb638
Leonid Gevorgyan <leonid.gevorgyan42@gmail.com>
2024-07-12 22:01:46 +0700

    fix: bg color

--- src/components/src/components/Terminal.jsx
+++ src/components/src/components/Terminal.jsx
@@ -3,7 +3,7 @@ import TextArea from "./TextArea";
 
 function Terminal() {
   return (
-    <div className="rounded-md mx-auto my-[250px] max-w-9xl sm:mx-[50px] md:mx-[100px] lg:mx-[600px]">
+    <div className="rounded-md mx-auto my-[250px] max-w-9xl sm:mx-[50px] md:mx-[100px] lg:mx-[200px]">
       <Header />
       <TextArea />
     </div>
